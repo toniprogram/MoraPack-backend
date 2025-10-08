27202ac6437c4f845a0d410b061dbb2d4ed707b9
Argedia <ariel2010gh@gmail.com>
2025-10-08 18:56:59 -0500

    iniciando

---
+++ src/main/java/com/morapack/models/Package.java
@@ -0,0 +1,17 @@
+package main.java.com.morapack.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "packages")
+public class Package {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+    private String origin;
+    private String destination;
+}
