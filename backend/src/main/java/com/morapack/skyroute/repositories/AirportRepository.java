8632020ebf838c950c665fbbeb6ed233bed4a8b1
Mueble2 <a20201531@pucp.edu.pe>
2025-11-04 01:36:57 -0500

    arreglando inconsistencias

--- backend/src/main/java/com/morapack/skyroute/repositories/AirportRepository.java
+++ backend/src/main/java/com/morapack/skyroute/repositories/AirportRepository.java
@@ -3,5 +3,5 @@ package com.morapack.skyroute.repositories;
 import com.morapack.skyroute.models.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AirportRepository extends JpaRepository<Airport, Long> {
+public interface AirportRepository extends JpaRepository<Airport, String> {
 }
